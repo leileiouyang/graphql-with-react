ce3c508a222d5fc265cb89a6a204dde8f30e24ce
Layla <43167521+Leilei-Ouyang@users.noreply.github.com>
2019-01-22 15:32:34 +0800

    graphql

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// connect to mlab database
+mongoose.connect('mongodb://Layla:biang_515@ds163054.mlab.com:63054/gql-react');
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+})
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true 
+}));
+
+app.listen(4000, () => {
+    console.log('now listening for requests on port 4000');
+})
