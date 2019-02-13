8625f662002cca10d457ab0a92d079fd4d6bf216
Layla <43167521+Leilei-Ouyang@users.noreply.github.com>
2019-02-13 15:48:19 +0800

    init commit

--- server/app.js
+++ server/app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+// allow cors-origin requests
+app.use(cors());
 
 // connect to mlab database
 mongoose.connect('mongodb://Layla:biang_515@ds163054.mlab.com:63054/gql-react');
